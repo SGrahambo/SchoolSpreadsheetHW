9c3d05a392afa321262020c9bb38b1b661c00902
Stephen Graham <stephen.graham@wsu.edu>
2023-09-22 12:02:50 -0700

    Tests added for new implementation of FibonacciTextReader.

--- FibonacciTests/FibonacciTests.cs
+++ FibonacciTests/FibonacciTests.cs
@@ -12,7 +12,24 @@ namespace HW3_Fibonacci.Tests
         public void Setup()
         {
         }
-        
+
+        [TestCase(1, "0")]
+        [TestCase(2, "1")]
+        [TestCase(3, "1")]
+        [TestCase(4, "2")]
+        [TestCase(5, "3")]
+        [TestCase(6, "5")]
+        [TestCase(11, "55")]
+        [TestCase(16, "610")]
+        [TestCase(51, "12586269025")]
+        [TestCase(101, "354224848179261915075")]
+        public void FibonacciTextReaderTest(int a, string b)
+        {
+            FibonacciTextReader ftr = new FibonacciTextReader(a);
+            string s = ftr.ReadToEnd();
+            s = s.Split(' ').Last();
+            Assert.That(s, Is.EqualTo(b));
+        }
 
         [TestCase(0, "0")]
         [TestCase(1, "1")]
