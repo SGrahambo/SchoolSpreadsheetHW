75554c4be96cfc5a9ee596a1a871cfd165af536a
Stephen Graham <stephen.graham@wsu.edu>
2023-10-03 22:07:51 -0700

    First commit. Stylecop added. InitializeDataGrid method created. CreateColumns method created and completed. Spent too long getting columns to go to ZZ (702 columns) rather than just Z (26). Will probably remove later if this overcomplicates things, but I really wanted to try it.

---
+++ Spreadsheet_Stephen_Graham/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Spreadsheet_Stephen_Graham
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.D = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.A,
+            this.B,
+            this.C,
+            this.D});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 426);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // A
+            // 
+            this.A.HeaderText = "A";
+            this.A.MinimumWidth = 6;
+            this.A.Name = "A";
+            this.A.Width = 125;
+            // 
+            // B
+            // 
+            this.B.HeaderText = "B";
+            this.B.MinimumWidth = 6;
+            this.B.Name = "B";
+            this.B.Width = 125;
+            // 
+            // C
+            // 
+            this.C.HeaderText = "C";
+            this.C.MinimumWidth = 6;
+            this.C.Name = "C";
+            this.C.Width = 125;
+            // 
+            // D
+            // 
+            this.D.HeaderText = "D";
+            this.D.MinimumWidth = 6;
+            this.D.Name = "D";
+            this.D.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Spreadsheet Cpts 321";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn D;
+    }
+}
+
---
+++ Spreadsheet_Stephen_Graham/Form1.cs
@@ -0,0 +1,90 @@
+﻿namespace Spreadsheet_Stephen_Graham
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.InitializeDataGrid(28, 26);
+        }
+
+        private void InitializeDataGrid(int columns, int rows)
+        {
+            this.CreateColumns(columns);
+            this.CreateRows(rows);
+        }
+
+        private void CreateColumns(int columns)
+        {
+            this.dataGridView1.Columns.Clear();
+
+            // Columns creation. I really wanted to have more than 26 columns. For now at least.
+            int columnCount = columns; // number of columns to generate.
+            int columnFillWeight = 100;
+            int maxColumnCount = 702; // 702 = "ZZ"
+
+            // prevents number of columns from going over the fillwidthlimit (65530) and going past column "ZZ"
+            if (columnCount > maxColumnCount)
+            {
+                columnCount = maxColumnCount;
+            }
+
+            if (columnCount > 655)
+            {
+                columnFillWeight = 93;
+            }
+
+            // Column generation
+            char columnName1 = 'A';
+            char columnName2 = '\0';
+
+            int cc = 0; // column counter
+            while (cc < columnCount && (cc < maxColumnCount))
+            {
+                while ((cc < columnCount) && (columnName1 <= 'Z') && (cc < maxColumnCount))
+                {
+                    this.dataGridView1.Columns.Add(columnName2.ToString() + columnName1.ToString(), columnName2.ToString() + columnName1.ToString());
+                    this.dataGridView1.Columns[cc].FillWeight = columnFillWeight;
+
+                    columnName1++;
+                    cc++;
+                }
+
+                if (cc == 26)
+                {
+                    columnName2 = 'A';
+                }
+                else
+                {
+                    columnName2++;
+                }
+
+                columnName1 = 'A';
+            }
+
+            // Column style
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.Font = new Font("Arial", 10, FontStyle.Bold);
+            columnHeaderStyle.Alignment = (DataGridViewContentAlignment)32;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+        }
+
+        private void CreateRows(int rows)
+        {
+            return;
+        }
+    }
+}
