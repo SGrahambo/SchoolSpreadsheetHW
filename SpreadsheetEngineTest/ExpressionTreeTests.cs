a92322c5bf0df4c8ae6bd73ad6bf056cf679fe00
Stephen Graham <stephen.graham@wsu.edu>
2023-10-17 14:51:44 -0700

    ExpressionTree class created. ExpressionTreeApp Project created. ExpressionTreeTests created.
    TDD.

---
+++ SpreadsheetEngineTest/ExpressionTreeTests.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="ExpressionTreeTests.cs" company="Stephen Graham - 011706998">
+// Copyright (c) Stephen Graham - 011706998. All rights reserved.
+// </copyright>
+
+namespace SpreadsheetEngine.Tests
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Tests the ExpressionTree class.
+    /// </summary>
+    public class ExpressionTreeTests
+    {
+        private ExpressionTree expressionTree;
+
+        [TestCase("5+1+2", 8)]
+        [TestCase("5-1-2", 2)]
+        [TestCase("5*1*2", 10)]
+        [TestCase("5/1/2", 2.5)]
+        [TestCase("5+1+-2", 4)]
+        [TestCase("5-1--2", 6)]
+        [TestCase("5*1*-2", -10)]
+        [TestCase("5/1/-2", -2.5)]
+        [TestCase("5+1+-2+0", 4)]
+        [TestCase("5-1--2-0", 6)]
+        [TestCase("5*1*-2*0", 0)]
+        [TestCase("5/1/-2/0", double.NegativeInfinity)]
+        [TestCase("5/1/2/0", double.PositiveInfinity)]
+        public void TestSingleOperators(string s, double d)
+        {
+            this.expressionTree = new ExpressionTree(s);
+            Assert.AreEqual(d, this.expressionTree.Evaluate());
+        }
+
+        [TestCase("5-3+1", 3)]
+        [TestCase("5+3-1*5-3+1", 1)]
+        [TestCase("10*5/2", 25)]
+        [TestCase("10/5*2", 4)]
+        [TestCase("6030/3*5+2", 12)]
+        [TestCase("60+30*3/5-2", 76)]
+        [TestCase("60/30-3+5*2", 9)]
+        [TestCase("60/30-3^3+5*2", -15)]
+        [TestCase("60*30+3-5/2", 1800.5)]
+        [TestCase("60*30+3-5/0", double.PositiveInfinity)]
+        [TestCase("4^3^2", 262144)]
+        public void TestOrderOfOperations(string s, double d)
+        {
+            this.expressionTree = new ExpressionTree(s);
+            Assert.AreEqual(d, this.expressionTree.Evaluate());
+        }
+
+        [TestCase("10+1/8-3+4*6", 31.125)]
+        [TestCase("10+1/8-3+(4*6)", 31.125)]
+        [TestCase("10+1/8-(3+(4*6))", -16.875)]
+        [TestCase("(10+1)/8-(3+(4*6))", -25.625)]
+        [TestCase("10+(1/8-3+4)*6", 16.75)]
+        [TestCase("(10+(1/(8-3)+4)*6)", 35.2)]
+        [TestCase("(((10+(1/(8-3)+4)*6)))", 35.2)]
+        public void TestOrderOfOperationsWithParenthesis(string s, double d)
+        {
+            this.expressionTree = new ExpressionTree(s);
+            Assert.AreEqual(d, this.expressionTree.Evaluate());
+        }
+    }
+}
---
+++ SpreadsheetEnginge/ExpressionTree.cs
@@ -0,0 +1,27 @@
+﻿namespace SpreadsheetEngine
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ExpressionTree
+    {
+        public ExpressionTree(string expression)
+        {
+            // TODO
+        }
+
+        public void SetVariable(string variableName, double variableValue)
+        {
+            // TODO
+        }
+
+        public double Evaluate()
+        {
+            // TODO
+            return 0;
+        }
+    }
+}
